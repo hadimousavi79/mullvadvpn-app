fa114b0cbe6624f54ee8d749344b24a0cdc213a3
Oliver <oliver@mohlin.dev>
2025-02-20 09:25:47 +0100

    Use as prop for polymorphism in Text

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/typography/Text.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/typography/Text.tsx
@@ -1,21 +1,17 @@
-import { createElement, forwardRef } from 'react';
-import styled, { WebTarget } from 'styled-components';
+import React from 'react';
+import styled, { PolymorphicComponentProps, WebTarget } from 'styled-components';
 
 import { Colors, Typography, typography, TypographyProperties } from '../../foundations';
 import { TransientProps } from '../../types';
 
-export type TextProps = React.PropsWithChildren<{
+export type Test = React.PropsWithChildren<{
   variant?: Typography;
   color?: Colors;
-  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
-  as?: WebTarget;
-  style?: React.CSSProperties;
 }>;
 
-const StyledText = styled(
-  ({ tag = 'span', ...props }: { tag: TextProps['tag'] } & TransientProps<TypographyProperties>) =>
-    createElement(tag, props),
-)((props) => ({
+export type TextProps<T extends WebTarget> = PolymorphicComponentProps<'web', Test, T, T>;
+
+const StyledText = styled.span<TransientProps<TypographyProperties>>((props) => ({
   color: 'var(--color)',
   fontFamily: props.$fontFamily,
   fontWeight: props.$fontWeight,
@@ -23,38 +19,30 @@ const StyledText = styled(
   lineHeight: props.$lineHeight,
 }));
 
-export const Text = forwardRef(
-  (
-    {
-      tag = 'span',
-      variant = 'bodySmall',
-      color = Colors.white,
-      children,
-      style,
-      ...props
-    }: TextProps,
-    ref,
-  ) => {
-    const { fontFamily, fontSize, fontWeight, lineHeight } = typography[variant];
-    return (
-      <StyledText
-        ref={ref}
-        tag={tag}
-        style={
-          {
-            '--color': color,
-            ...style,
-          } as React.CSSProperties
-        }
-        $fontFamily={fontFamily}
-        $fontWeight={fontWeight}
-        $fontSize={fontSize}
-        $lineHeight={lineHeight}
-        {...props}>
-        {children}
-      </StyledText>
-    );
-  },
-);
+export const Text = <T extends WebTarget>({
+  variant = 'bodySmall',
+  color = Colors.white,
+  children,
+  style,
+  ...props
+}: TextProps<T>) => {
+  const { fontFamily, fontSize, fontWeight, lineHeight } = typography[variant];
+  return (
+    <StyledText
+      style={
+        {
+          '--color': color,
+          ...style,
+        } as React.CSSProperties
+      }
+      $fontFamily={fontFamily}
+      $fontWeight={fontWeight}
+      $fontSize={fontSize}
+      $lineHeight={lineHeight}
+      {...props}>
+      {children}
+    </StyledText>
+  );
+};
 
 Text.displayName = 'Text';
